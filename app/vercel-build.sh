67ba0b382612f1f1d800351e11ba79730ed45610
malleswaritirumalaFWAi <malleswari.tirumala@freedomwithai.com>
2026-06-17 16:22:34 +0530

    Update vercel-build.sh

--- app/vercel-build.sh
+++ app/vercel-build.sh
@@ -4,4 +4,4 @@ if cd flutter; then git pull && cd ..; else git clone https://github.com/flutter
 flutter/bin/flutter doctor
 flutter/bin/flutter config --enable-web
 flutter/bin/flutter pub get
-flutter/bin/flutter build web --release --dart-define=API_BASE=$API_BASE --dart-define=RAZORPAY_KEY_ID=$RAZORPAY_KEY_ID
+flutter/bin/flutter build web --release --dart-define=API_BASE=$API_BASE --dart-define=RAZORPAY_KEY_ID=$RAZORPAY_KEY_ID --dart-define=DEMO_MODE=$DEMO_MODE
